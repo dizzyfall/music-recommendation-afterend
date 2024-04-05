14244abbb52d1e9a02e9ee65c92ad95f0e5a43be
dizzyfall <791700642@qq.com>
2024-04-05 13:46:12 +0800

    update 优化歌曲表字段

--- src/main/java/com/dzy/mapper/SongMapper.java
+++ src/main/java/com/dzy/mapper/SongMapper.java
@@ -1,15 +1,15 @@
 package com.dzy.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.dzy.model.entity.Song;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
- * @author DZY
- * @description 针对表【song(歌曲表)】的数据库操作Mapper
- * @createDate 2024-04-03 11:40:52
- * @Entity generator.domain.Song
- */
+* @author DZY
+* @description 针对表【song(歌曲表)】的数据库操作Mapper
+* @createDate 2024-04-05 13:41:01
+* @Entity generator.domain.Song
+*/
 @Mapper
 public interface SongMapper extends BaseMapper<Song> {
 
--- src/main/java/com/dzy/model/entity/Song.java
+++ src/main/java/com/dzy/model/entity/Song.java
@@ -1,130 +1,129 @@
 package com.dzy.model.entity;
 
 import com.baomidou.mybatisplus.annotation.*;
-import lombok.Data;
-import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 import java.util.Date;
+import lombok.Data;
+import org.springframework.stereotype.Component;
 
 /**
  * 歌曲表
- *
  * @TableName song
  */
-@TableName(value = "song")
+@TableName(value ="song")
 @Data
 @Component
 public class Song implements Serializable {
-
     /**
      * 歌曲id
      */
-    @TableId(value = "song_id", type = IdType.AUTO)
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     /**
      * 歌曲歌手id列表
      */
-    @TableField(value = "song_singer_list_id")
+    @TableField(value = "singer_list_id")
     private String singerListId;
 
     /**
      * 歌曲作词者id
      */
-    @TableField(value = "song_writer_id")
+    @TableField(value = "writer_id")
     private Long writerId;
 
     /**
      * 歌曲作曲者id
      */
-    @TableField(value = "song_composer_id")
+    @TableField(value = "composer_id")
     private Long composerId;
 
     /**
      * 歌曲专辑id,歌曲没有专辑默认为-1
      */
-    @TableField(value = "song_album_id")
+    @TableField(value = "album_id")
     private Long albumId;
 
     /**
      * 歌曲歌词id
      */
-    @TableField(value = "song_lyric_id")
+    @TableField(value = "lyric_id")
     private Long lyricId;
 
     /**
      * 歌曲封面保存路径,歌曲有专辑使用专辑图片作为歌曲封面否则使用歌手头像
      */
-    @TableField(value = "song_image_path")
+    @TableField(value = "image_path")
     private String imagePath;
 
     /**
      * 歌曲名
      */
-    @TableField(value = "song_title")
+    @TableField(value = "title")
     private String title;
 
     /**
      * 歌曲简介
      */
-    @TableField(value = "song_description")
+    @TableField(value = "description")
     private String description;
 
     /**
      * 歌曲语种
      */
-    @TableField(value = "song_lang")
+    @TableField(value = "lang")
     private String lang;
 
     /**
      * 歌曲流派
      */
-    @TableField(value = "song_genre")
+    @TableField(value = "genre")
     private String genre;
 
     /**
      * 歌曲收藏数量
      */
-    @TableField(value = "song_collection_count")
-    private Integer collectionCount;
+    @TableField(value = "collect_count")
+    private Integer collectCount;
 
     /**
      * 歌曲点赞数量
      */
-    @TableField(value = "song_favour_count")
+    @TableField(value = "favour_count")
     private Integer favourCount;
 
     /**
      * 歌曲评论数量
      */
-    @TableField(value = "song_comment_count")
+    @TableField(value = "comment_count")
     private Integer commentCount;
 
     /**
      * 歌曲发行时间
      */
-    @TableField(value = "song_publish_time")
+    @TableField(value = "publish_time")
     private Date publishTime;
 
     /**
      * 歌曲创建时间
      */
-    @TableField(value = "song_create_time")
+    @TableField(value = "create_time")
     private Date createTime;
 
     /**
      * 歌曲更新时间
      */
-    @TableField(value = "song_update_time")
+    @TableField(value = "update_time")
     private Date updateTime;
 
     /**
      * 逻辑删除
      */
-    @TableField(value = "song_is_delete")
+    @TableField(value = "is_delete")
     @TableLogic
     private Integer isDelete;
 
-    private static final long serialVersionUID = -1924436440097193684L;
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
 }

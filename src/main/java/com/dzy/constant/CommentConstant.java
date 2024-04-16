4ead338003e30ad49a2021cc08c78e73fad31aa3
dizzyfall <791700642@qq.com>
2024-04-16 16:43:54 +0800

    add 新增评论类型常量功能

---
+++ src/main/java/com/dzy/constant/CommentConstant.java
@@ -0,0 +1,17 @@
+package com.dzy.constant;
+
+/**
+ * 评论类型常量
+ *
+ * @Author <a href="https://github.com/dizzyfall">DZY</a>
+ * @Date 2024/4/16  10:31
+ */
+public interface CommentConstant {
+
+    String SONG_TYPE = "song";
+
+    String ALBUM_TYPE = "album";
+
+    String SONGLIST_TYPE = "songlist";
+
+}
